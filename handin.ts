f3c3d3bd567ec9ce36e5cb4a7fe0c4c50d6b2a5a
Adeel Bhutta <a.bhutta@northeastern.edu>
2026-05-09 20:33:51 -0400

    updated handin to clarify tasks

--- handin.ts
+++ handin.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 /*
 TASK 1: Give five cURL commands that make HTTP requests to the URL
@@ -10,10 +10,13 @@ https://name-seeing-server.onrender.com
  - A POST request that returns the data `{ "error": "Incorrect key", correctPrefix: 4 }`
  - A request that returns status 500 (Internal server error) by causing a JavaScript exception
 
+ - Tip: Use curl -i to see HTTP status codes
+
 The code for that website is at
 https://github.com/neu-se/name-seeing-server/blob/main/server.ts
 
 */
+
 
 /* TASK 2: What is the correct Zod type for verifying input to the POST route?
  * Replacing the current `zPostBody` validator with this validator should
@@ -21,19 +24,19 @@ https://github.com/neu-se/name-seeing-server/blob/main/server.ts
  * should also ensure that the `namesPreviouslySeen` set will only ever
  * contain strings.
  */
-const zPostBody = z.any();
+const zPostBody = z.object();
 
 /* TASK 3: Give a Zod type describing the possible *responses* to `GET /`
  * requests
  */
-const zGetResponse = z.any();
+const zGetResponse = z.object();
 
 /* TASK 4: Give a Zod type describing the possible *responses* to `POST /`
  * requests when the status is 200
  */
-const zPostResponse = z.any();
+const zPostResponse = z.object();
 
 /* TASK 5: Give a Zod type describing the possible *responses* to `POST /`
  * requests when the status is 400 or 403
  */
-const zPostErrorResponse = z.any();
+const zPostErrorResponse = z.union();
